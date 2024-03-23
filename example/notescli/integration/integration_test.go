e0835df15ac867636ce7530f3f9d037467583c13
Soroush Javadi <soroush.javadi@gmail.com>
2024-03-23 12:04:41 +0330

    enhance example integration test

--- example/notescli/integration/integration_test.go
+++ example/notescli/integration/integration_test.go
@@ -2,10 +2,7 @@ package integration_test
 
 import (
 	"context"
-	"fmt"
-	"log"
 	"net"
-	"os"
 	"testing"
 
 	"github.com/soroushj/grpcmock"
@@ -17,43 +14,27 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-const (
-	port = "5050"
-)
-
-var (
-	mock = grpcmock.New()
-)
-
-func TestMain(m *testing.M) {
-	// Create a gRPC server using the mock interceptor
+func TestNotesCLI(t *testing.T) {
+	// Create a gRPC server using a grpcmock interceptor
+	mock := grpcmock.New()
 	server := grpc.NewServer(grpc.UnaryInterceptor(mock.UnaryServerInterceptor()))
+	defer server.Stop()
 	// Register the generated Unimplemented implementation of the Notes server
 	notes.RegisterNotesServer(server, &notes.UnimplementedNotesServer{})
-	// Run the server
-	addr := fmt.Sprintf(":%v", port)
-	lis, err := net.Listen("tcp", addr)
+	// Run the server on an available port
+	lis, err := net.Listen("tcp", ":0")
 	if err != nil {
-		log.Fatalf("failed to listen at %v: %v", addr, err)
+		t.Fatalf("listen: %v", err)
 	}
 	go func() {
 		if err := server.Serve(lis); err != nil {
-			log.Fatalf("failed to serve at %v: %v", addr, err)
+			t.Errorf("serve at %v: %v", lis.Addr(), err)
 		}
 	}()
-	// Run the tests
-	code := m.Run()
-	// Stop the server and exit
-	server.Stop()
-	os.Exit(code)
-}
-
-func TestGetNoteText(t *testing.T) {
 	// Connect to the mock Notes server
-	addr := fmt.Sprintf("localhost:%v", port)
-	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.Dial(lis.Addr().String(), grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		t.Fatalf("failed to dial %v: %v", addr, err)
+		t.Fatalf("dial %v: %v", lis.Addr(), err)
 	}
 	defer conn.Close()
 	// Create a Notes client
@@ -61,51 +42,53 @@ func TestGetNoteText(t *testing.T) {
 	// Create a NotesCLI to be tested
 	nc := notescli.New(client)
 	ctx := context.Background()
-	// Set a mock handler for the GetNote method on the Notes server
-	mock.SetHandler("GetNote", func(ctx context.Context, req interface{}) (interface{}, error) {
-		r := req.(*notes.GetNoteRequest)
-		if r.Id == "1" {
-			return &notes.GetNoteResponse{
-				Note: &notes.Note{
-					Id:   "1",
-					Text: "test",
-				},
-			}, nil
-		}
-		return nil, status.Error(codes.NotFound, "note not found")
-	})
-	// Run the subtests
-	testCases := []struct {
-		name    string
-		id      string
-		text    string
-		errCode codes.Code
-	}{
-		{"Note exists", "1", "test", codes.OK},
-		{"Note does not exist", "2", "", codes.NotFound},
-	}
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			text, err := nc.GetNoteText(ctx, tc.id)
-			if text != tc.text {
-				t.Errorf("text: got %q want %q", text, tc.text)
+	t.Run("GetNoteText", func(t *testing.T) {
+		// Set a mock handler for the GetNote method on the Notes server
+		mock.SetHandler("GetNote", func(ctx context.Context, req interface{}) (interface{}, error) {
+			r := req.(*notes.GetNoteRequest)
+			if r.Id == "1" {
+				return &notes.GetNoteResponse{
+					Note: &notes.Note{
+						Id:   "1",
+						Text: "a",
+					},
+				}, nil
 			}
-			if err, _ := status.FromError(err); err.Code() != tc.errCode {
-				t.Errorf("err: got %v want %v", err.Code(), tc.errCode)
+			return nil, status.Error(codes.NotFound, "note not found")
+		})
+		// Run the subtests
+		testCases := []struct {
+			name    string
+			id      string
+			text    string
+			errCode codes.Code
+		}{
+			{"Found", "1", "a", codes.OK},
+			{"Not found", "2", "", codes.NotFound},
+		}
+		for _, tc := range testCases {
+			t.Run(tc.name, func(t *testing.T) {
+				text, err := nc.GetNoteText(ctx, tc.id)
+				if text != tc.text {
+					t.Errorf("text: got %q want %q", text, tc.text)
+				}
+				if err, _ := status.FromError(err); err.Code() != tc.errCode {
+					t.Errorf("err: got %v want %v", err.Code(), tc.errCode)
+				}
+			})
+		}
+		// Set a mock response for another test case
+		mock.SetResponse("GetNote", &grpcmock.UnaryResponse{
+			Resp: &notes.GetNoteResponse{},
+		})
+		t.Run("Bad response", func(t *testing.T) {
+			text, err := nc.GetNoteText(ctx, "3")
+			if text != "" {
+				t.Errorf("text: got %q want %q", text, "")
+			}
+			if err != notescli.ErrBadResponse {
+				t.Errorf("err: got %v want %v", err, notescli.ErrBadResponse)
 			}
 		})
-	}
-	// Set a mock response for another test case
-	mock.SetResponse("GetNote", &grpcmock.UnaryResponse{
-		Resp: &notes.GetNoteResponse{},
-	})
-	t.Run("Bad response", func(t *testing.T) {
-		text, err := nc.GetNoteText(ctx, "3")
-		if text != "" {
-			t.Errorf("text: got %q want %q", text, "")
-		}
-		if err != notescli.ErrBadResponse {
-			t.Errorf("err: got %v want %v", err, notescli.ErrBadResponse)
-		}
 	})
 }
